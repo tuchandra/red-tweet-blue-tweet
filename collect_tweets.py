f692c15c35383e527cbc99640a048ea871ef744f
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2017-04-29 17:22:13 -0500

    Use tweepy instead of twitter

--- collect_tweets.py
+++ collect_tweets.py
@@ -31,10 +31,56 @@ if __name__ == "__main__":
     consumer_key, consumer_secret, access_token, access_secret = read_credentials()
     stream = TwitterStream(auth=OAuth(access_token, access_secret,
                                       consumer_key, consumer_secret))
+import json
+import os
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+def read_credentials():
+    secret_file = os.path.expanduser('credentials.json')
+    
+    if not os.path.exists(secret_file):
+        raise Exception("Credentials file not found.")
+
+    with open(secret_file) as f:
+        secret_contents = json.loads(f.read())
+
+    # Unpack contents. Format of file:
+    # { "consumer_key" : "...",
+    #   "consumer_secret" : "...",
+    #   "access_token" : "...",
+    #   "access_secret" : "..."
+    # }
+
+    consumer_key = secret_contents["consumer_key"]
+    consumer_secret = secret_contents["consumer_secret"]
+    access_token = secret_contents["access_token"]
+    access_secret = secret_contents["access_secret"]
+
+    return consumer_key, consumer_secret, access_token, access_secret
+
+class StdOutListener(StreamListener):
+    """Listener that prints tweets as they arrive."""
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+
+if __name__ == "__main__":
+    consumer_key, consumer_secret, access_token, access_secret = read_credentials()
+
+    listener = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    stream = Stream(auth, listener)
 
     keywords = ["climate", "#climatemarch", "global warming"]
-    twitterator = stream.statuses.filter(track = ["#climatemarch"])
-
-
-    for tweet in twitterator:
-        print(tweet)
+    twitterator = stream.filter(track = ["#climatemarch"])
