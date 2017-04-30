7d2d37509dcdd90850941154e95f0c93a4d6948e
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2017-04-30 15:10:05 -0500

    Change keywords

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,39 +1,6 @@
 import json
 import os
 
-from twitter import OAuth, TwitterStream
-from twitter.util import printNicely
-
-def read_credentials():
-    secret_file = os.path.expanduser('credentials.json')
-    
-    if not os.path.exists(secret_file):
-        raise Exception("Credentials file not found.")
-
-    with open(secret_file) as f:
-        secret_contents = json.loads(f.read())
-
-    # Unpack contents. Format of file:
-    # { "consumer_key" : "...",
-    #   "consumer_secret" : "...",
-    #   "access_token" : "...",
-    #   "access_secret" : "..."
-    # }
-
-    consumer_key = secret_contents["consumer_key"]
-    consumer_secret = secret_contents["consumer_secret"]
-    access_token = secret_contents["access_token"]
-    access_secret = secret_contents["access_secret"]
-
-    return consumer_key, consumer_secret, access_token, access_secret
-
-if __name__ == "__main__":
-    consumer_key, consumer_secret, access_token, access_secret = read_credentials()
-    stream = TwitterStream(auth=OAuth(access_token, access_secret,
-                                      consumer_key, consumer_secret))
-import json
-import os
-
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -82,5 +49,5 @@ if __name__ == "__main__":
 
     stream = Stream(auth, listener)
 
-    keywords = ["climate", "#climatemarch", "global warming"]
-    twitterator = stream.filter(track = ["#climatemarch"])
+    keywords = ["trump", "#trump"]
+    twitterator = stream.filter(track = keywords)
