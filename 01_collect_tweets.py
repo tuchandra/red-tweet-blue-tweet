7934b77cd250efab0146812b397c869a7299fd5d
Tushar Chandra <TusharChandra2018@u.northwestern.edu>
2017-05-01 04:31:27 -0500

    Rewrite IO logic to read and write from files
    01_collect_tweets.py now writes tweets to JSON files that will simply
    contain collections of tweets. 02_extract_users.py reads these JSON files
    and writes CSV files of the user IDs of the authors of these tweets.

---
+++ 01_collect_tweets.py
@@ -0,0 +1,49 @@
+import time
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+from auth import authorize
+
+
+class FileOutputListener(StreamListener):
+    """Listener that stores tweets to JSON file as they arrive.
+
+    time_limit: amount of time (seconds) to keep the stream open
+    output_file: JSON file to save tweets to
+    """
+
+    def __init__(self, time_limit, output_file):
+        self.start_time = time.time()
+        self.time_limit = time_limit
+        self.output_file = output_file
+
+
+    def on_data(self, tweet):
+        """On each tweet, print it out while the stream is still active."""
+
+        if (time.time() - self.start_time) < self.time_limit:
+            self.output_file.write(str(tweet))
+            return True
+
+        else:
+            return False
+
+    def on_error(self, status):
+        """Print error messages when they appear."""
+        print(status)
+
+
+if __name__ == "__main__":
+    # Setup output file
+    t = time.strftime("%m%d_%H%M%S", time.localtime())
+    with open("{0}.json".format(t), "a") as output_file:
+        # Create and authorize listener
+        listener = FileOutputListener(10, output_file)
+        auth = authorize()
+        stream = Stream(auth, listener)
+
+        # Filter tweets with keywords
+        keywords = ["trump"]
+        twitterator = stream.filter(track = keywords)
---
+++ 02_extract_users.py
@@ -0,0 +1,51 @@
+"""Extract authors of all of the collected tweets. Apply activity filters,
+only keeping users for whom all of the following holds:
+ - at least 25 followers
+ - at least 100 statuses
+ - at least 100 followed users
+ - tweet in english
+"""
+
+import csv
+import json
+
+import tweepy
+
+from auth import authorize
+
+if __name__ == "__main__":
+    # Each JSON file is a collection of tweets.
+    files = ["0501_042904.json", "0501_042924.json"]
+
+    # From each tweet collection, get users of interest
+    for fname in files:
+        user_set = set()
+
+        with open(fname) as f:
+            # Each line is a tweet or empty. Attempt to decode it as JSON,
+            # and create set (no duplicates) of UserIDs who tweeted and meet
+            # other activity conditions.
+            for line in f:
+                try:
+                    l = json.loads(line)
+                    user = l["user"]
+
+                    # Since we have access to the user here, exclude those
+                    # with fewer than 25 followers, 100 statuses, 100
+                    # followed users, or those who don't tweet in english.
+                    if (user["followers_count"] < 25 or 
+                        user["statuses_count"] < 100 or 
+                        user["friends_count"] < 100 or
+                        user["lang"] != "en"):
+                        continue
+
+                    user_set.add(user["id_str"])
+
+                except:
+                    continue
+
+            csv_name = "users_" + fname.split(".")[0] + ".csv"
+            with open(csv_name, "w") as csv_file:
+                cw = csv.writer(csv_file)
+                for user  in user_set:
+                    cw.writerow([user,])
