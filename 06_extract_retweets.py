967ba652f8b09059a94f80200d908ccf2ee92e0d
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2017-06-05 17:28:35 -0500

    Initial commit

---
+++ 06_extract_retweets.py
@@ -0,0 +1,54 @@
+"""DO THIS"""
+
+import csv
+import time
+
+import pymongo
+
+def export_retweets(client, collection_name):
+
+    col = client["twitter"][collection_name]
+
+    retweets = {}
+
+    for i, tweet in enumerate(col.find({"retweeted_status": {"$exists" : True}}, no_cursor_timeout = True)):
+        # Extract date and IDs of original user / retweeting user
+        try:
+            timestamp = time.strptime(tweet["created_at"], "%a %b %d %H:%M:%S +0000 %Y")
+            tweet_date = time.strftime("%Y_%m_%d", timestamp)
+
+            retweeter = tweet["user"]["id_str"]
+            retweeted = tweet["retweeted_status"]["user"]["id_str"]
+
+            tweet_info = (tweet_date, retweeter, retweeted)
+        except:
+            continue
+
+        # Let retweets be a dictionary that has keys date (YY_MM_DD), and
+        # values lists of retweet triples (date, retweeter, retweeted)
+        try:
+            retweets[tweet_date].append(tweet_info)
+        except KeyError:
+            retweets[tweet_date] = [tweet_info]
+
+    # Write all of the retweet lists to files.
+    for date, tweets in retweets.items():
+        fname = "retweets_{0}.csv".format(date)
+        with open(fname, "w") as f:
+            writer = csv.writer(f)
+            for tweet in tweets:
+                try:
+                    writer.writerow(tweet)
+                except:
+                    continue
+
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient("localhost", 27017)
+    export_retweets(client, "tweets_filtered")
+
+
+
+
+
+
