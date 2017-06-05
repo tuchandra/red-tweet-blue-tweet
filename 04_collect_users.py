fcc8d1ccb46a61d2d9730174404218e554aa4bf6
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2017-06-05 17:27:34 -0500

    Fix bug with users list resetting

--- 04_collect_users.py
+++ 04_collect_users.py
@@ -9,33 +9,34 @@ import csv
 
 import pymongo
 
-
 def export_userlist(client, collection_name):
     """Get all users in collection and export as CSV."""
 
-    data = client["twitter"][collection_name].find()
+    data = client["twitter"][collection_name].find(no_cursor_timeout = True)
     count = data.count()
 
     i = 0
+    users = {}
     for tweet in data:
         # Track processed documents (enumerate didn't work for some reason)
         i += 1
 
-        users = {}
-
         # Maintain users as hashmap of (user:number of tweets)
         try:
             users[tweet["user"]["id_str"]] += 1
-        except:
+        except Exception as e:
             users[tweet["user"]["id_str"]] = 1
 
         if i % 50000 == 0:
-            print("Processed {0}/{1} tweets".format(i, count))
+            print("Processed {0}/{1} tweets; have {2} users".format(i, count, len(users.keys())))
 
     with open("user_list.csv", "w") as outfile:
         writer = csv.writer(csv_file)
         for user_id, num_tweets in users.items():
-           writer.writerow([user_id, num_tweets])
+            try:
+                writer.writerow([user_id, num_tweets])
+            except:
+                continue
 
     return
 
