431d51c62375ab5a56a2b8808cd5de67800817f2
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2017-06-07 01:18:46 -0500

    Add row projections

--- 08_estimation.r
+++ 08_estimation.r
@@ -51,3 +51,60 @@ col.df <- data.frame(
     stringsAsFactors = FALSE)
 
 save(col.df, file="col_coord.Rdata")
+
+
+#===============================================================
+# PROJECT ROWS
+#
+# UNCLEAR
+#===============================================================
+
+# Note: We cannot simply take res$rowcoord, because we didn't use all of
+# the rows in computing the SVD (we used those who followed at least 5
+# politicians).
+
+load("adj_matrix.Rdata")  # reloads entire y
+load("col_coord.Rdata")
+
+# We can only project users who follow at least one of our politicians.
+y <- y[rowSums(y)>0,]  # (472,985 x 443)
+
+# Load column coordinates, but remove labels (443 x 3)
+colcoords <- matrix(as.matrix(col.df[,2:4]), ncol=3)
+
+# Matrix of singular values (472985, 3)
+# [[sigma1, sigma2, sigma3],
+#  [sigma1, sigma2, sigma3],
+#             ...          ]
+svphi <- matrix(res$sv[1:3], nrow = nrow(y), ncol = 3, byrow = TRUE)
+
+cs <- colmasses  # length 443
+gam.00 <- colcoords  # (443 x 3)
+
+points <- as.matrix(y) * 1  # (472985 x 443), and the *1 coerces to numeric
+rs.sum <- rowSums(points)  # length 472985
+
+# Normalize points so that rows sum to 1, then transpose (so columns sum to 1)
+base2 <- points / matrix(rs.sum, nrow = nrow(points), ncol = ncol(points))
+base2 <- t(base2)
+
+# Prepare to project
+cs.0 <- matrix(cs, nrow = nrow(base2), ncol = ncol(base2))  # (443 x 472985)
+base2 <- base2 - cs.0  # (443 x 472985)
+
+# Do the projection
+# t(as.matrix(base2)) has dim (472985 x 443); gam.00 has dim (443 x 3)
+# multiplying gives (472985 x 3); then divide by singular values svphi
+phi <- t(as.matrix(base2)) %*% gam.00 / svphi
+row.df <- phi
+
+# Save projection matrix
+row.df <- data.frame(
+    rowname = rownames(y)[1:nrow(row.df)],
+    coord1 = row.df[,1],
+    coord2 = row.df[,2],
+    coord3 = row.df[,3],
+    sum = rowSums(y),
+    stringsAsFactors = FALSE)
+
+save(row.df, file='output/row_coord.Rdata')
