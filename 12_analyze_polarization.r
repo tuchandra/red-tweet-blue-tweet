5c42f6e0c3f0f5c4bdd2b3f711817701939a0b77
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2017-06-07 15:54:14 -0500

    Plot polarization measures

--- 12_analyze_polarization.r
+++ 12_analyze_polarization.r
@@ -45,9 +45,33 @@ for (i in 1 : length(files)) {
 
 }
 
+
 #===============================================================
 # PLOT POLARIZATION INDEXES
 # For each measure of polarization, plot it and save the result.
 #===============================================================
 
-# tba
+
+df1 <- data.frame(pol1 = pol1.days, day = c(1:19))
+plt <- ggplot(df1, aes(x=day, y=pol1)) + geom_point() + geom_smooth() +
+       labs(x="Date", y="Proportion of Pairs", 
+            title="Proportion of pairs with same-direction ideology") +
+       scale_x_continuous(labels = c("May 10", "May 20", "May 29"), breaks = c(1, 10, 19))
+ggsave(filename = "plots/polarize1.png", plot = plt, width=6, height=4)
+
+
+df2 <- data.frame(pol2 = pol2.days, day = c(1:19))
+plt <- ggplot(df2, aes(x=day, y=pol2)) + geom_point() + geom_smooth() +
+       labs(x="Date", y="Proportion of Pairs", 
+            title="Proportion of pairs within 1 standard deviation of each other") +
+       scale_x_continuous(labels = c("May 10", "May 20", "May 29"), breaks = c(1, 10, 19))
+ggsave(filename = "plots/polarize2.png", plot = plt, width=6, height=4)
+
+
+df3 <- data.frame(pol3 = pol3.days, day = c(1:19))
+plt <- ggplot(df3, aes(x=day, y=pol3)) + geom_point() + geom_smooth() +
+       labs(x="Date", y="Polarization Measure", 
+            title="Average polarization of retweeted information") +
+       scale_x_continuous(labels = c("May 10", "May 20", "May 29"), breaks = c(1, 10, 19)) +
+       scale_y_continuous(breaks = seq(1.0, 1.3, by=0.05))
+ggsave(filename = "plots/polarize3.png", plot = plt, width=6, height=4)
